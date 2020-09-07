9dc4e61ad249ecac0de64d82f2e335fccb80c2e3
Carlos <albertocarlos221@gmail.com>
2020-09-07 01:38:02 -0300

    criacao do App com Vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,82 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="limitador cabecalho">
+      <h1 class="text-center">Conversor de Moedas</h1>
+    </div>
+    <div id="conversor">
+      <div class="linha">    
+      <Conversor moeda-a="USD" moeda-b="BRL"/>
+      <Conversor moeda-a="BRL" moeda-b="USD"/>
+    </div>
+    <div class="linha">
+      <Conversor moeda-a="CAD" moeda-b="BRL"/>
+      <Conversor moeda-a="BRL" moeda-b="CAD"/>
+    </div>
+    <div class="linha">
+      <Conversor moeda-a="EUR" moeda-b="BRL"/>
+      <Conversor moeda-a="BRL" moeda-b="EUR"/>
+    </div>
+    </div>
+    <div class="limitador rodape">
+      <h3 class="text-center">Desenvolvido com <span class="fas fa-heart"></span> por <a href="https://www.instagram.com/sameoldcarlos">Carlos Alves</a></h3>
+    </div>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import Conversor from "./components/Conversor.vue"
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Conversor
   }
 }
 </script>
 
 <style>
-#app {
+#app {    
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background: repeating-linear-gradient(
+45deg,
+#dbdbdb,
+#dbdbdb 3px,
+#a3a3a3 3px,
+#a3a3a3 6px)
+}
+a, a:hover{
+  color: #ffffff;
+  text-decoration: none;
+}
+.limitador {
+  top: 0;
+  width: 100%;
+  height: 80px;
+  background-color: #000000;
+  color: #ffffff;
+  display: table;
+}
+
+#conversor {
+  margin: 60px auto;
+  max-width: 900px;
+}
+.linha {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 20px;
+  margin-bottom: 30px;
+  
+}
+
+.text-center {
+  vertical-align: middle;
+  text-align: center;
+  display:table-cell;
+  
 }
 </style>
---
+++ src/components/Conversor.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="conversor">
+    <h2 id="de-para">{{moedaA}} Para {{moedaB}}</h2>
+    <input type="number" v-model="moedaA_value" :placeholder="moedaA" @keyup="converter">
+    <button type="button" class="btn btn-success" @click="converter">
+        <span class="fas fa-exchange-alt"></span>
+    </button>
+    <h2>{{moedaB_value}}</h2>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Conversor',
+  props: ["moedaA", "moedaB"],
+        data(){
+            return{
+                moedaA_value : "",
+                moedaB_value : 0
+            };
+        },
+        methods: {            
+            converter(){
+                if(this.moedaA_value == ""){
+                    this.moedaB_value = 0;
+
+                }
+                else{
+                let de_para = this.moedaA + "_" + this.moedaB;
+
+                let url = "https://free.currconv.com/api/v7/convert?q="+
+                    de_para
+                +"&compact=y&apiKey=c7052d5be45cda8ce00a"
+
+                fetch(url)
+                    .then(res=>{
+                        return res.json()
+                    })
+                    .then(json=>{
+                        let cotacao = json[de_para].val;
+                        this.moedaB_value = (cotacao * parseFloat(this.moedaA_value)).toFixed(2);
+                          });
+            }
+            }
+
+        },
+}
+</script>
+
+<style scoped>
+#de-para {
+    color: #28a745;
+}
+.conversor {
+    margin: 0 auto;
+    padding: 20px;
+    max-width: 300px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    background-color: #fff;
+}
+
+.btn {
+    margin-left: 10px;
+}
+
+</style>>
